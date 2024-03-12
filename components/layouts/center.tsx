04bc3eea7564c86f9a278b05e4ab94e73be03a44
bovir456 <162837906+bovir456@users.noreply.github.com>
2024-03-12 21:19:39 +0700

    center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -1,26 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Center = ({ children }: { children: React.ReactNode }) => {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = "//pl22770665.profitablegatecpm.com/765b268fbc09e33e4c2583d48437ce36/invoke.js";
-        script.setAttribute("data-cfasync", "false");
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
-
     return (
-        <div id="container-765b268fbc09e33e4c2583d48437ce36">
-                </div>
+     <script async="async" data-cfasync="false" src="//pl22770665.profitablegatecpm.com/765b268fbc09e33e4c2583d48437ce36/invoke.js"></script>
+<div id="container-765b268fbc09e33e4c2583d48437ce36"></div>
         <div className="flex flex-col h-[50vh] justify-center items-center">
             {children}
-            
         </div>
     );
 };
 
-export default Center;
+export default Center
