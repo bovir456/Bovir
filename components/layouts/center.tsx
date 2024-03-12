f35d9e54ec7ccd998aa38ff332db1ffda81477e6
bovir456 <162837906+bovir456@users.noreply.github.com>
2024-03-12 21:27:59 +0700

    center.tsx

--- components/layouts/center.tsx
+++ components/layouts/center.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Center = ({ children }: { children: React.ReactNode }) => {
     return (
-     <script async="async" data-cfasync="false" src="//pl22770665.profitablegatecpm.com/765b268fbc09e33e4c2583d48437ce36/invoke.js"></script>
-<div id="container-765b268fbc09e33e4c2583d48437ce36"></div>
-        <div className="flex flex-col h-[50vh] justify-center items-center">
+      <div className="flex flex-col h-[50vh] justify-center items-center">
             {children}
         </div>
     );
